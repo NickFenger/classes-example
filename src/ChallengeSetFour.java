1c37e9872fef66637c1cbf94c92b0a6545de3bba
Nick Fenger <nfenger@pps.net>
2019-01-15 10:47:23 -0800

    peer test 2

--- src/ChallengeSetFour.java
+++ src/ChallengeSetFour.java
@@ -15,6 +15,8 @@ public class ChallengeSetFour {
 
     //peer test 1
 
+
+    //peer test 2
     public void guessingGame() {
         while (5 != 4) {
             int randNum = (int) Math.floor(Math.random() * 1001);
